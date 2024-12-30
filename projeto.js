29b48e10201b3d276d6fadabee84a4457cb2e0dc
Lauranasc <llauranasc@hotmail.com>
2024-12-30 18:05:19 -0300

    projetodio

--- projeto.js
+++ projeto.js
@@ -1,1 +1,31 @@
-console.log("projeto")
+let nomeDoHeroi = "Zé";
+let xp = 11000;
+let mensagem = " "
+
+if (xp <= 1000) {
+   mensagem = "O Heroi de nome "+ nomeDoHeroi + " esta no nivel de Ferro"
+    
+} else if(xp >= 1001 && xp <= 2000){
+    mensagem = "O Heroi de nome "+ nomeDoHeroi + " esta no nivel de Bronze"
+
+} else if(xp >= 2001 && xp <= 5000 ){
+    mensagem = "O Heroi de nome "+ nomeDoHeroi + " esta no nivel de Prata"
+
+} else if(xp >= 5001 && xp <= 7000){
+    mensagem = "O Heroi de nome "+ nomeDoHeroi + " esta no nivel de Ouro"
+
+} else if(xp >= 7001 && xp <= 8000){
+    mensagem = "O Heroi de nome " + nomeDoHeroi + " esta no nivel de Platina "
+
+} else if (xp >= 8001 && xp <= 9000){
+    mensagem = "O Heroi de nome "+ nomeDoHeroi + " esta no nivel de Ferro Ascendente"
+
+} else if (xp >= 9001 && xp <= 10000){
+    mensagem = "O Heroi de nome "+ nomeDoHeroi + " esta no nivel de Imortal"
+
+} else {
+    mensagem = "O Heroi de nome "+ nomeDoHeroi + "esta no nivel de Radiante"
+}
+
+console.log(mensagem);
+
