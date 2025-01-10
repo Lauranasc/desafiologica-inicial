5edabf1e9c14b15c3dbf1afcf26622233a22ca12
Lauranasc <llauranasc@hotmail.com>
2025-01-10 14:56:53 -0300

    projeto classes de um jogo

---
+++ aulaObjects.js
@@ -0,0 +1,27 @@
+class heroi{
+        ataque = " ";
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+
+    }
+    atacar(){
+        if (this.tipo === "mago"){
+            this.ataque = "magia"
+        } else if (this.tipo === "guerreiro"){
+            this.ataque = "espada"
+        } else if (this.tipo === "monge"){
+            this.ataque = "artes marciais"
+        } else if (this.tipo === "ninja") {
+          this.ataque = "shuriken"
+        } else {
+            this.ataque = " tipo invalido"
+        }
+        console.log(`o ${this.tipo} ${this.nome}, de ${this.idade} anos, atacou usando: ${this.ataque}`)
+    }
+    
+
+}
+let heroiPropriedades = new heroi("shifu", 140, "ninja");
+heroiPropriedades.atacar()
